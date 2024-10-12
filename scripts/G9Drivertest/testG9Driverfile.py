afc011a4402a1d5758aa9ff5d64cf8e9ff16a70b
Mark Ruzicka <marker11778@gmail.com>
2024-10-12 17:28:52 -0500

    working to see if the driver file works

---
+++ scripts/G9Drivertest/testG9Driverfile.py
@@ -0,0 +1,25 @@
+import g9_driver
+import time
+
+g9 = g9_driver(port='COM11')
+
+responses = []
+
+for _ in range(5):
+    try:
+        g9.send_command() 
+
+    except ConnectionError as e:
+        print("ConnectionError:", e)
+    except ValueError as e:
+        print("ValueError:", e)
+
+    print(g9.lastResponse)
+    responses.append(g9.lastResponse)
+
+    time.sleep(1)
+
+
+with open("test.txt", "w") as f:
+    for response in responses:
+        f.write(f"{response}\n") 
