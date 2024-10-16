72d95c9d641920aecff1978b2509c75c6ea46864
Arundhati Singh <arundhatisingh171003@gmail.com>
2024-10-16 14:39:58 -0500

    temp

--- subsystem/interlocks.py
+++ subsystem/interlocks.py
@@ -18,6 +18,7 @@ def resource_path(relative_path):
     try:
         # PyInstaller creates a temp folder and stores path in _MEIPASS
         base_path = sys._MEIPASS
+        
     except AttributeError:
         base_path = os.path.abspath(".")
 
