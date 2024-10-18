c6b7bef94d5303e9d6bc7bb6f258a5ba6b8eff5b
Mark Ruzicka <marker11778@gmail.com>
2024-10-18 15:31:51 -0500

    pushing for tests

--- instrumentctl/g9_driver.py
+++ instrumentctl/g9_driver.py
@@ -122,15 +122,12 @@ class G9Driver:
 
                 # Terminal Status Flags
                 SITSF = data[21:27]
-                print(SITSF)
                 if not self.check_flags13(SITSF):
                     if self.safety_in_terminal_error(data[31:55][-10:]):
                         raise ValueError("Error was detected in inputs but was not found")
                     
                     
-                
                 SOTDF = data[17:21]
-                
                 if not self.check_flags13(SOTDF):
                     err = []
                     gates = self.bytes_to_binary(SOTDF[-2:])
