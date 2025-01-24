d5c506782b11670fb0f3578708a46679f2181350
Mark Ruzicka <marker11778@gmail.com>
2025-01-24 10:49:37 -0600

    testing again

---
+++ test2.py
@@ -0,0 +1,9 @@
+class testingsomeBADcode:
+    
+    def __init__(self, time, arg1, arg3):
+        self.time = time
+        self.Arg1 = arg1
+    #asdfasdf
+    def gettime(self):
+        self.gotted = True
+        return self.time
