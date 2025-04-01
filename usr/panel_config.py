6f12120dc2c3e29cd9f7905aa8e5aadcaad5fff4
Arundhati Singh <arundhatisingh171003@gmail.com>
2025-04-01 17:48:06 -0500

    removed spaces from panel and comport config files

--- usr/panel_config.py
+++ usr/panel_config.py
@@ -1,9 +1,7 @@
 # usr/panel_config.py
 import json
 
-
 CONFIG_FILE = 'usr/usr_data/pane_state.json'
-
 
 # saves all the data related to the frames/widgets and panes to a file
 def save_pane_states(config, frames, pane, filepath=CONFIG_FILE):
@@ -12,14 +10,11 @@ def save_pane_states(config, frames, pane, filepath=CONFIG_FILE):
    for i in range(_ - 1):
        data[f'paned_{i}'] = pane.sash_coord(i)
 
-
    for item in config:
        data[item[0]] = (frames[item[0]].winfo_width(), frames[item[0]].winfo_height())
 
-
    with open(filepath, 'w') as file:
        json.dump(data, file)
-
 
 # reads in file and passes the config file back as a dict
 def load_pane_states(filepath=CONFIG_FILE):
@@ -31,8 +26,7 @@ def load_pane_states(filepath=CONFIG_FILE):
        print("No previous pane state saved.")
    except Exception as e:
        print(f"Failed to load pane states: {e}")
-
-
+       
 # checks to see if that config file exists
 def saveFileExists(filepath=CONFIG_FILE):
    try:
