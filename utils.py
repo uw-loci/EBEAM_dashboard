6f475c0acdb459ba4afa8370521803e693a808b7
Arundhati Singh <arundhatisingh171003@gmail.com>
2025-08-14 13:07:47 -0500

    cleaning up the branch to prepare for pull request

--- utils.py
+++ utils.py
@@ -19,9 +19,8 @@ class LogLevel(enum.IntEnum):
     CRITICAL = 5
 
 class Logger:
-    def __init__(self, text_widget, file_log_level = LogLevel.DEBUG, log_level=LogLevel.INFO, log_to_file=False):
+    def __init__(self, text_widget, log_level=LogLevel.INFO, log_to_file=False):
         self.text_widget = text_widget
-        self.file_log_level = file_log_level
         self.log_level = log_level
         self.log_to_file = log_to_file
         self.log_file = None
@@ -211,7 +210,7 @@ class MessagesFrame:
         )
 
         self.file_logging_enabled = True
-        self.logger = Logger(self.text_widget, log_level=LogLevel.DEBUG, file_log_level=LogLevel.DEBUG, log_to_file=True)
+        self.logger = Logger(self.text_widget, log_level=LogLevel.DEBUG, log_to_file=True)
 
         # Redirect stdout to the text widget
         sys.stdout = TextRedirector(self.text_widget, "stdout")
@@ -258,9 +257,6 @@ class MessagesFrame:
 
     def get_log_level(self):
         return self.logger.log_level
-    
-    def get_file_log_level(self):
-        return self.logger.file_log_level
 
     def flush(self):
         """ Flush method needed for stdout redirection compatibility. """
