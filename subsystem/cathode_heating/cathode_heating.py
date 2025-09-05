240f723149c77d81181f13aca830869455d63efe
MathomJohnson <156151500+MathomJohnson@users.noreply.github.com>
2025-09-04 22:13:16 -0500

    Added beams off button to GUI

--- subsystem/cathode_heating/cathode_heating.py
+++ subsystem/cathode_heating/cathode_heating.py
@@ -193,6 +193,7 @@ class CathodeHeatingSubsystem:
         style.configure('OverTemp.TLabel', foreground='red', font=('Helvetica', 10, 'bold'))  # Overtemperature style
         style.configure('RampOn.TButton', background='green', foreground='black', font=('Helvetica', 8, 'bold'))
         style.configure('RampOff.TButton', background='red', foreground='black', font=('Helvetica', 8, 'bold')) # Ramp button style
+        style.configure('BeamsOff.TButton', background='red', foreground='red', font=('Helvetica', 12, 'bold'))
 
         # Load toggle images
         self.toggle_on_image = tk.PhotoImage(file=resource_path("media/toggle_on.png"))
@@ -456,6 +457,15 @@ class CathodeHeatingSubsystem:
         # Ensure the grid layout of config_tab accommodates the new buttons
         config_tab.columnconfigure(0, weight=1)
         config_tab.columnconfigure(1, weight=1)
+
+        # Beams Off safety button
+        self.beams_off_button = ttk.Button(
+            self.parent,
+            text="Beams OFF",
+            style='BeamsOff.TButton',
+            command=self.turn_all_beams_off
+        )
+        self.beams_off_button.pack(side="top", fill="x", padx=10, pady=8)
 
         self.init_time = datetime.datetime.now()
 
