8105aa0cafcea7e6a3e3242f5af964e0d007309e
sbasu107 <sbasu107@gmail.com>
2026-03-13 17:55:28 -0500

    remove echoback test and temp reading buttons again

--- subsystem/cathode_heating/cathode_heating.py
+++ subsystem/cathode_heating/cathode_heating.py
@@ -953,14 +953,7 @@ class CathodeHeatingSubsystem:
             overtemp_status_label.pack(side='left')
             ttk.Label(overtemp_status_frame, textvariable=self.overtemp_status_vars[i], style='Bold.TLabel').pack(side='left', padx=(8, 0))
 
-            # Place echoback and temperature buttons on the config tab
-            echoback_button = ttk.Button(config_tab, text=f"Perform Echoback Test Unit {i+1}",
-                                        command=lambda unit=i+1: self.perform_echoback_test(unit))
-            echoback_button.grid(row=12, column=0, columnspan=2, sticky='w', pady=(4, 2))
 
-            read_temp_button = ttk.Button(config_tab, text=f"Read Temperature Unit {i+1}",
-                                        command=lambda unit=i+1: self.read_and_log_temperature(unit))
-            read_temp_button.grid(row=13, column=0, columnspan=2, sticky='w', pady=(2, 2))
 
 
         # Ensure the grid layout of config_tab accommodates the new buttons
