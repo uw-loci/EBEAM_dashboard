9113ce02d789646f952eb9ce8519f2692ddd069a
Daniel Egelhoff <degelhoff@wisc.edu>
2026-03-20 16:10:46 -0500

    Added pymodbus logging to the tester file.

--- instrumentctl/knob_box/pymodbus_tester.py
+++ instrumentctl/knob_box/pymodbus_tester.py
@@ -1,9 +1,13 @@
 from pymodbus.client import ModbusSerialClient
 import time
+import logging
+logging.basicConfig()
+logging.getLogger("pymodbus").setLevel(logging.DEBUG)
+logging.getLogger("pymodbus.transaction").setLevel(logging.DEBUG)
 
 PORT = "COM13"      # Change if needed
 BAUDRATE = 9600
-SLAVE_IDS = [4]
+SLAVE_IDS = [1,2,3,4]
 
 client = ModbusSerialClient(
     port=PORT,
@@ -11,7 +15,7 @@ client = ModbusSerialClient(
     bytesize=8,
     parity='N',
     stopbits=1,
-    timeout=2
+    timeout=0.3
 )
 
 if not client.connect():
@@ -26,7 +30,7 @@ try:
 
     input_regs_ok = 0
     discretes_ok = 0
-    total_reads = 100
+    total_reads = 10
 
     for i in range(total_reads):
         # ---- Read 5 Input Registers (0-4) ----
@@ -36,12 +40,16 @@ try:
             slave=slave_id
         )
 
-        # if rr.isError():
-        #     print(f"[{i + 1}/{total_reads}] Input register error:", rr)
-        if not rr.isError():
+        if rr is None:
+            print("No response")
+        elif rr.isError():
+            print("Error response:", rr)
+        else:
             input_regs_ok += 1
+            raw = rr.encode()            # bytes
+            print("Raw bytes:", raw.hex())
 
-        time.sleep(0.5)
+        time.sleep(0.1)
 
         # ---- Read 19 Registers (5-23) ----
         rr = client.read_input_registers(
@@ -50,13 +58,14 @@ try:
             slave=slave_id
         )
 
-        # if rr.isError():
-        #     print(f"[{i + 1}/{total_reads}] Discrete input error:", rr)
-        # else:
-        if not rr.isError():
-            discretes_ok += 1
-
-        time.sleep(0.1)
+        if rr is None:
+            print("No response")
+        elif rr.isError():
+            print("Error response:", rr)
+        else:
+            discretes_ok
+            raw = rr.encode()            # bytes
+            print("Raw bytes:", raw.hex())
 
     input_regs_pct = (input_regs_ok / total_reads) * 100.0
     discretes_pct = (discretes_ok / total_reads) * 100.0
