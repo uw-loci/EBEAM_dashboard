db761d61605a830649ccc1a10fd51a96f9a410b3
Daniel Egelhoff <degelhoff@wisc.edu>
2026-05-04 15:29:15 -0500

    The temperature controller stop_reading function was not being called, causing COM port errors to be printed on shutdown.

--- subsystem/cathode_heating/cathode_heating.py
+++ subsystem/cathode_heating/cathode_heating.py
@@ -2990,7 +2990,8 @@ class CathodeHeatingSubsystem:
 
         if hasattr(self, 'temperature_controller') and self.temperature_controller:
             try:
-               # self.temperature_controller.stop_reading()
+                self.temperature_controller.stop_reading()
+                self.temp_controllers_connected = False
                 self.temperature_controller.disconnect()
             except Exception as e:
                 self.log(f"Error cleaning up existing controller: {str(e)}", LogLevel.ERROR)
