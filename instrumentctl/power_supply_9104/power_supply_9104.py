1893e94fd193e6c99ab8d943aa0348ba0e02a74e
Ott <cott@Morgridge.net>
2026-06-10 11:35:15 -0500

    Changed disable_output() to use 1.5s lock timeout instead of default .5s

--- instrumentctl/power_supply_9104/power_supply_9104.py
+++ instrumentctl/power_supply_9104/power_supply_9104.py
@@ -819,7 +819,7 @@ class PowerSupply9104:
         command = f"SETM{setv1:04}{seti1:04}{swtime1:03}{setv2:04}{seti2:04}{swtime2:03}{setv3:04}{seti3:04}{swtime3:03}"
         return self.send_command(command)
     
-    def disable_output(self, lock_timeout=None):
+    def disable_output(self, lock_timeout=1.5):
         """Disable the power supply output unconditionally (no OVP validation)."""
         command = "SOUT0"
         response = self.send_command(command, lock_timeout=lock_timeout)
